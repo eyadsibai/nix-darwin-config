1cd80ed69bd5ef3287898b65ecf5f18d253ef4a3
eyadsibai <eyad.alsibai@gmail.com>
2024-08-14 21:57:26 +0300

    update

--- home/default.nix
+++ home/default.nix
@@ -33,6 +33,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.file.".config/.aerospace.toml" = ./aerospace.toml;
+  home.file.".config/.aerospace.toml" = {source = ./aerospace.toml;};
 
 }
