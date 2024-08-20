3b9762c686b816e791045e09c775cdbb3e9e36a2
eyadsibai <eyad.alsibai@gmail.com>
2024-08-20 14:35:50 +0300

    update

--- home/sketchybar/items/spaces.sh
+++ home/sketchybar/items/spaces.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-SPACE_SIDS=(1 2 3 4 5 6 7 8 9 10)
+SPACE_SIDS=$(/opt/homebrew/bin/aerospace list-workspaces --all);
+
+sketchybar --add event aerospace_workspace_change
+
 
 for sid in "${SPACE_SIDS[@]}"
 do
-  sketchybar --add space space.$sid left                                 \
-             --set space.$sid space=$sid                                 \
+  sketchybar --add space space.$sid left \
+          --subscribe space.$sid aerospace_workspace_change \
+                                 \
+             --set space.$sid                                \
                               icon=$sid                                  \
-                              label.font="sketchybar-app-font:Regular:16.0" \
+                              label.font="sketchybar-app-font:Regular:14.0" \
                               label.padding_right=20                     \
                               label.y_offset=-1                          \
-                              script="$PLUGIN_DIR/space.sh"              
+                              script="$PLUGIN_DIR/space.sh"             \
+                              click_script="/opt/homebrew/bin/aerospace workspace $sid"               
 done
 
-sketchybar --add item space_separator left                             \
+sketchybar --add item space_separator left             \                \
            --set space_separator icon="􀆊"                                \
                                  icon.color=$ACCENT_COLOR \
                                  icon.padding_left=4                   \
